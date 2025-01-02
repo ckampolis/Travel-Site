f7300832d0153ecbf357eab0f21f8bfb805f2ecc
ckampolis <costask200@gmail.com>
2025-01-02 14:06:15 -0500

    Initial commit

---
+++ personal-website/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+function App() {
+  return (
+  
+  <>
+      <Navbar />
+        <Routes>
+          <Route  path='/' exact element={<Home />} />
+          <Route  path='/services' exact element={<Services />} />
+          <Route  path='/products' exact element={<Products/>} />
+          <Route  path='/sign-up' exact element={<SignUp />} />
+        </Routes>
+  </>
+);
+}
+
+export default App;
